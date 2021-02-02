dd96c5c0ac5ab3f1597040123ed13a474bb59859
Kola Antoshkiv <ak.pkqtor@gmail.com>
2021-02-02 19:13:30 +0200

    Fix bug with icons

--- assets/js/data/soc_net.js
+++ assets/js/data/soc_net.js
@@ -1,5 +1,5 @@
 
 const SUPPORTED_SOCIA_NET = new Map();
 SUPPORTED_SOCIA_NET.set('www.facebook.com', './assets/icons/facebook.svg');
-SUPPORTED_SOCIA_NET.set('twitter.com', './assets/icons/instagram.svg');
-SUPPORTED_SOCIA_NET.set('www.instagram.com', './assets/icons/twitter.svg');
+SUPPORTED_SOCIA_NET.set('twitter.com', './assets/icons/twitter.svg');
+SUPPORTED_SOCIA_NET.set('www.instagram.com', './assets/icons/instagram.svg');
